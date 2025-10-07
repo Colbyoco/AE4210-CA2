8eca7d51a932476aaeac232e422a14088f0d659b
colbyoco <colbyoco@gmail.com>
2025-10-07 15:41:31 -0400

    Edits

--- CA2_Const_CP.m
+++ CA2_Const_CP.m
@@ -1,1 +1,4 @@
+%CA2 Constant CP full run
 
+%C1
+%[P2,T2] = inlet_rev_con_cp(T1,P1,M1)
--- inlet_rev_con_cp.m
+++ inlet_rev_con_cp.m
@@ -4,7 +4,7 @@ function [P2,T2] = inlet_rev_con_cp(T1,P1,M1)
 gam = 1.4; %Constant Cp case
 
 %Using Isentropic flow equations...
-Pt = P1*(1+(gam-1)*M1^2/2)^gam/(gam-1);
+Pt = P1*(1+(gam-1)*M1^2/2)^(gam/(gam-1));
 Tt = T1*(1+(gam-1)*M1^2/2);
 
 P2 = Pt;
