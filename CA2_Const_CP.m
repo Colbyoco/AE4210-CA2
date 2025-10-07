223f9a832be0907d406bce38766e26b1b62cb1dd
colbyoco <colbyoco@gmail.com>
2025-10-07 16:10:57 -0400

    ryan

--- CA2_Const_CP.m
+++ CA2_Const_CP.m
@@ -3,3 +3,11 @@
 %C1
 %[P2,T2] = inlet_rev_con_cp(T1,P1,M1)
 
+%C3
+%Define known turbojet parameters
+T4 = 1450+273; %K
+M0 = 2;
+%At 16.2 km altitude, from textbook appendix,
+T0 = 216.6; %K
+P0 = (7565-10350)/(18-16)*.2 + 10350; %Pa, interpolated from table
+
