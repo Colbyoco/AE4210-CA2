65e42055a354d9f46739939bf11a54daad8fb135
Ryan Smith <sryana879@gmail.com>
2025-10-07 16:06:23 -0400

    ye

--- nozzle_rev_con_cp.m
+++ nozzle_rev_con_cp.m
@@ -1,5 +1,19 @@
-function [Pe, Te, Me] = nozzle_rev_con_cp(Tt, Pt, M)
-%Trying to get thre properties at the exit of the nozzle
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function [P9, T9, M9] = nozzle_rev_con_cp(P5, T5, P0)
+%Modeling Exhaust in the Nozzle from 5-9
+%Outputs of P9, T9, M9
+%Inputs of P5, T5, M5
+
+gam = 1.4;
+
+%P5 and T5 are stagnation temperatures
+Pt5 = P5;
+Tt5 = T5;
+P9 = P0;
+
+%Now solving for M9 using pressure equation
+M9 = (((Pt5/P9)^((gam-1)/gam)-1)*(2/(gam-1)))^0.5;
+
+%Rearranging Temperature equation to get T9
+T9 = Tt5/(1+(gam-1)*M9^2/2);
+
 end
