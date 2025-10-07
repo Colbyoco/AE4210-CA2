65ede2da98ca1d3b8fa1a83a11732c316d52ef1f
sryana879-cabb <sryana879@gmail.com>
2025-10-07 14:56:17 -0400

    Add files via upload

---
+++ inlet_rev_con_cp.m
@@ -0,0 +1,12 @@
+function [P2,T2] = inlet_rev_con_cp(T1,P1,M1)
+%Modeling an inlet diffuser (0-2) with initial inputs P0, T0, M)
+%To get P2 and T2
+gam = 1.4; %Constant Cp case
+
+%Using Isentropic flow equations...
+Pt = P1*(1+(gam-1)*M1^2/2)^gam/(gam-1);
+Tt = T1*(1+(gam-1)*M1^2/2);
+
+P2 = Pt;
+T2 = Tt;
+end
