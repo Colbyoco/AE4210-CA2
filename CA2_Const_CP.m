6d696f564d03866abf8720d4b7bcc825ec6bfbcd
colbyoco <colbyoco@gmail.com>
2025-10-09 00:42:42 -0400

    Lowkey done

--- CA2_Const_CP.m
+++ CA2_Const_CP.m
@@ -36,11 +36,15 @@ np_list = zeros(1, 371);
 no_list = zeros(1, 371);
 ST_list = zeros(1, 371);
 phi_list = zeros(1, 371);
-
+nth_list_var = zeros(1, 371);
+np_list_var = zeros(1, 371);
+no_list_var = zeros(1, 371);
+ST_list_var = zeros(1, 371);
+phi_list_var = zeros(1, 371);
 %Use for loop to get efficiencies, equivalence ratio, and specific thrust
 %for every pi_c value
 [P2,T2] = inlet_rev_con_cp(T0,P0,M0);
-
+[T2_var, P2_var] = inlet_rev_var_cp(T0,P0, M0);
 for i = 1:length(pi_c)
 
 [T3_rev, W_rev_c] = comp_rev_low_ma_con_cp(pi_c(i), T2, Cp_con);
@@ -71,29 +75,69 @@ nth_list(i) = ((1+f)*v9^2 - v0^2)/(2*Qc);
 np_list(i) = ST_list(i)*v0*2/((1+f)*v9^2-v0^2); %Lecture 7
 
 no_list(i) = nth_list(i)*np_list(i);
+
+%we can do the same for variable Cp for the same range of Pi_c
+
+[T3_rev_var, W_rev_c_var] = comp_rev_low_ma_var_cp(P2_var, pi_c(i), T2_var);
+
+[phi_list_var(i),f] = comb_rev_low_mach_var_cp(T3_rev_var,T4);
+
+P3_var = pi_c(i) * P2_var;
+P4_var = P3_var;
+pi_t_guess = pi_c(i)^.8;
+work_balance = @(pi_t) work_error_var(pi_t, P4_var ,T4, W_rev_c_var);
+pi_t = fsolve(work_balance, pi_t_guess);
+
+P5_var = P4_var/pi_t;
+[T5_rev_var,~] = turb_rev_low_ma_var_cp(pi_t,P4_var, T4);
+
+[P9, T9, M9, v9] = nozzle_rev_var_cp(P5_var, T5_rev_var, P0);
+
+ST_list_var(i) = (1+f)*v9 - v0;
+%Then we get thermal efficiency, propulsive efficiency, and overall
+%efficiency
+Qc = delta_h_var_cp(T3_rev_var, T4)/.029;     %J/mol*(1mol/.029kg)
+nth_list_var(i) = ((1+f)*v9^2 - v0^2)/(2*Qc);
+
+np_list_var(i) = ST_list_var(i)*v0*2/((1+f)*v9^2-v0^2); %Lecture 7
+
+no_list_var(i) = nth_list_var(i)*np_list_var(i);
 end
 
+%Plotting Specific Thrust
 subplot(2,1,1)
-plot(pi_c, ST_list)
+plot(pi_c, ST_list,'r')
 title('Specific Thrust at 16.2 km and M=2')
 xlabel('Compressor Pressure Ratio')
 ylabel('Specific Thrust, N/(kg/s)')
+hold on
+plot(pi_c, ST_list_var,'--','Color','red')
+hold off
+legend('Constant Cp', 'Variable Cp')
 
 subplot(2,1,2)
-plot(pi_c,no_list)
+plot(pi_c,no_list,'b')
 hold on
-plot(pi_c,nth_list)
-plot(pi_c,np_list)
-title('Specific Thrust at 16.2 km and M=2')
+plot(pi_c,nth_list,'r')
+plot(pi_c,np_list,'g')
+title('Efficiencies, solid - constant Cp, dotted - variable Cp')
 xlabel('Compressor Pressure Ratio')
 ylabel('Efficiencies')
 axis([0 40 .2 1])
+plot(pi_c,no_list_var,'--','Color','b')
+plot(pi_c,nth_list_var,'--','Color','r')
+plot(pi_c,np_list_var,'--','Color','g')
+legend('overall efficiency','thermal efficiency','propulsive efficiency')
 
+%C3 part 2 answer for variable and constant Cp
 [ST_max_const, max_const_idx] = max(ST_list)
 max_pi_const = pi_c(max_const_idx)
 phi_const = phi_list(max_const_idx)
 
-%We can do the same for 
+[ST_max_var, max_var_idx] = max(ST_list_var)
+max_pi_var = pi_c(max_var_idx)
+phi_var = phi_list(max_var_idx)
+
 
 %For C3 part 3, we know the static conditions and pi_c = 15.5
 T4 = 1450+273; %K
@@ -137,17 +181,26 @@ const_err = (thrust_const - 139400)/139400
 
 %for variable cp, C3 part 3
 
-[T2, P2] = inlet_rev_var_cp(T0,P0, M0)
-[T3_rev, W_rev] = comp_rev_low_ma_var_cp(P2, pi_c, T2)
-[phi,f] = comb_rev_low_mach_var_cp(T3,T4)
+[T2_var, P2_var] = inlet_rev_var_cp(T0,P0, M0);
 
-pi_t_guess = pi_c(i)^.8;
-work_balance = @(pi_t) work_error_var(pi_t, T4, Cp_con, W_rev_c);
+[T3_rev_var, W_rev_c_var] = comp_rev_low_ma_var_cp(P2_var, pi_c_conc, T2_var);
+
+[phi,f] = comb_rev_low_mach_var_cp(T3_rev_var,T4);
+
+P3_var = pi_c_conc * P2_var;
+P4_var = P3_var;
+pi_t_guess = pi_c_conc^.8;
+work_balance = @(pi_t) work_error_var(pi_t, P4_var ,T4, W_rev_c_var);
 pi_t = fsolve(work_balance, pi_t_guess);
-[T5_rev,W_rev] = turb_rev_low_ma_var_cp(pi_t,P4, T4)
-nozzle_rev_con_cp
 
-ST_conc = (1+f)*v9 - v0;
 
-thrust_const = ST_conc * 186
+P5_var = P4_var/pi_t;
+[T5_rev_var,~] = turb_rev_low_ma_var_cp(pi_t,P4_var, T4);
 
+[P9, T9, M9, v9] = nozzle_rev_var_cp(P5_var, T5_rev_var, P0);
+
+ST_conc_var = (1+f)*v9 - v0;
+
+thrust_var = ST_conc_var * 186
+
+var_err = (thrust_var - 139400)/139400
